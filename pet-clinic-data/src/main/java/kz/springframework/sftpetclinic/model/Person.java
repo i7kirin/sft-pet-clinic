89a34c28167da9d110e22c54709933c4c23e364c
Duman <zhanbolatov.duman@inessoft.kz>
2019-05-17 17:21:45 +0600

    wtf? I have commit it yet!

--- pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package kz.springframework.sftpetclinic.model;
 
-public class Person extends BaseEntity{
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package kz.springframework.sftpetclinic.model;
 
-public class PetType extends BaseEntity{
+public class PetType extends BaseEntity {
 
     private String name;
 
