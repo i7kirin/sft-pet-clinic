e908ebbfa85adeed23f85af4d51ddfe0a3ad80ab
Duman <zhanbolatov.duman@inessoft.kz>
2019-05-17 17:29:01 +0600

    Refactored Interfaces to Base Interface. closes #11

--- pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package kz.springframework.sftpetclinic.services;
 
 import kz.springframework.sftpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService  extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/services/VetService.java
@@ -1,15 +1,6 @@
 package kz.springframework.sftpetclinic.services;
 
-import kz.springframework.sftpetclinic.model.Pet;
 import kz.springframework.sftpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Pet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
