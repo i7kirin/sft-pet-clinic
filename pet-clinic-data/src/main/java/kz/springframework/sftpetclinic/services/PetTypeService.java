a01ee75216d166a09ddf8aeb3d6a9a179f00f653
Duman <zhanbolatov.duman@inessoft.kz>
2019-06-17 11:17:29 +0600

    added PetTypeService service. closes #29

---
+++ pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package kz.springframework.sftpetclinic.services;
+
+import kz.springframework.sftpetclinic.model.PetType;
+
+public interface PetTypeService extends  CrudService<PetType, Long> {
+}
