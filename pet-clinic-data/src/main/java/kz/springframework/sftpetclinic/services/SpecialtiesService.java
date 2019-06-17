8e95db3ec90df6fbe93e9b8e05e46517534723ee
Duman <zhanbolatov.duman@inessoft.kz>
2019-06-17 12:12:06 +0600

    Added Specialties Map Impl. closes #31

---
+++ pet-clinic-data/src/main/java/kz/springframework/sftpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package kz.springframework.sftpetclinic.services;
+
+import kz.springframework.sftpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
